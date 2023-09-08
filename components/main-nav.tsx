7d22b8f9f72677cbfbe0cdf727a42b657e69776d
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-09-07 23:54:47 -0300

    Update main-nav.tsx

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -27,6 +27,26 @@ export default function MainNav({
             label: "Categorias", 
             active: pathname === `/${params.storeId}/categories`
         },
+        {
+            href: `/${params.storeId}/sizes`,
+            label: "Tamanhos", 
+            active: pathname === `/${params.storeId}/sizes`
+        },
+        {
+            href: `/${params.storeId}/colors`,
+            label: "Cores", 
+            active: pathname === `/${params.storeId}/colors`
+        },
+        {
+            href: `/${params.storeId}/products`,
+            label: "Produtos", 
+            active: pathname === `/${params.storeId}/products`
+        },
+        {
+            href: `/${params.storeId}/orders`,
+            label: "Pedidos", 
+            active: pathname === `/${params.storeId}/orders`
+        },
         { 
             href: `/${params.storeId}/settings`,
             label: "Ajustes",
