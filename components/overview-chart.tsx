a1b9e9a2f30428252da55151c5a2199a665d6e10
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-09-23 22:45:16 -0300

    add dashboard statistics and chart

---
+++ components/overview-chart.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { Bar, BarChart, CartesianGrid, ResponsiveContainer, XAxis, YAxis } from "recharts"
+
+type OverviewChartProps = {
+    data: any[]
+};
+
+export default function OverviewChart({ data }: OverviewChartProps) {
+    return (
+        <ResponsiveContainer width="100%" height={350}>
+            <BarChart data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis 
+                    dataKey="name" 
+                    stroke="#888888"
+                    fontSize={12}
+                    tickLine={false}
+                    axisLine={false}
+                />
+                <YAxis 
+                    stroke="#888888"
+                    fontSize={12}
+                    tickLine={false}
+                    axisLine={false}
+                    tickFormatter={(value) => `$${value}`}
+                />
+                
+                <Bar dataKey="total" fill="#3498db" radius={[4, 4, 0, 0]} />
+            </BarChart>
+        </ResponsiveContainer>
+    )
+}
